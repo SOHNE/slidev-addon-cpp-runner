ce9e646ed9b573fae41d9ab963b5f601678dbd58
Leandro Peres <leandroperes@protonmail.com>
2025-04-01 09:12:47 -0300

    chore: add `setup/code-runner.ts`

---
+++ setup/code-runners.ts
@@ -0,0 +1,8 @@
+import { CodeRunnerOutput, defineCodeRunnersSetup } from '@slidev/types';
+import { ref } from 'vue';
+import { useNav } from '@slidev/client';
+
+export default defineCodeRunnersSetup(() => {
+  return {};
+});
+
